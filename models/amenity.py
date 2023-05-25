a37034c80a2d68a1b89b352e2d50e1c566399ee5
VBamurange <v.bamurange@alustudent.com>
2023-05-25 16:02:53 +0200

    inheritance changes

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''amenity'''
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''city'''
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    state_id = ""
+    name = ""
