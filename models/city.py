429780370c95587ab9b200dc02836df9a73506b9
TetaDory <d.rutayisir1@alustudent.com>
2023-05-26 00:00:28 -0700

    changes

--- models/city.py
+++ models/city.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """
         Class to manage city objects
-        Represent a city with a state id and a name
+        Represent a city with state id and name
         Attributes:
             state_id (str): state id of the city
             name (str): name of the city
